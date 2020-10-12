172e43d58c7bdf2c7343f263cdb7dd9a9f0c3431
Darbyblinko <jacobcrombie@live.com>
2020-10-12 16:03:52 -0600

    intial commit

---
+++ Controllers/GameController.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace rockpaperscissors.Controllers
+{
+  class GameController
+  {
+    public bool Playing { get; set; } = true;
+    public void Launch()
+    {
+      while (Playing)
+      {
+        
+      }
+    }
+
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using rockpaperscissors.Controllers;
+using System.Threading;
+
+namespace rockpaperscissors
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      Console.BackgroundColor = ConsoleColor.Black;
+      Console.ForegroundColor = ConsoleColor.Green;
+      System.Console.WriteLine(@"
+ (                        (                                (                                 
+ )\ )              )      )\ )                             )\ )                              
+(()/(           ( /(     (()/(    )           (   (       (()/(     (                (       
+ /(_)) (    (   )\())     /(_))( /(  `  )    ))\  )(       /(_)) (  )\  (   (    (   )(   (  
+(_))   )\   )\ ((_)\     (_))  )(_)) /(/(   /((_)(()\     (_))   )\((_) )\  )\   )\ (()\  )\ 
+| _ \ ((_) ((_)| |(_) )  | _ \((_)_ ((_)_\ (_))   ((_) )  / __| ((_)(_)((_)((_) ((_) ((_)((_)
+|   // _ \/ _| | / / /(  |  _// _` || '_ \)/ -_) | '_|/(  \__ \/ _| | |(_-<(_-</ _ \| '_|(_-<
+|_|_\\___/\__| |_\_\(_)) |_|  \__,_|| .__/ \___| |_| (_)) |___/\__| |_|/__//__/\___/|_|  /__/
+                                    |_|                                                      
+      ");
+      Thread.Sleep(1000);
+      System.Console.WriteLine("Rock, Paper, or Scissors, choose wisely.");
+      var choice = Console.ReadLine();
+      GameController gc = new GameController();
+      gc.Launch();
+    }
+  }
+}
